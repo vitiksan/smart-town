9dcc9a8e50bee095e40bca6df3c057ab6342e00e
Vitalii Makh <vmakh@agileassets.com>
2022-05-12 00:43:43 +0300

    Update structure to multi modules project

---
+++ monitor-emulator/src/main/java/com/team/blue/Emulator.java
@@ -0,0 +1,13 @@
+package com.team.blue;
+
+/**
+ * Hello world!
+ *
+ */
+public class Emulator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
